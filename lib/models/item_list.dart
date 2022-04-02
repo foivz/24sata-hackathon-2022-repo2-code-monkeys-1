477201aac563ddc3514542dedcf7052f6c34f3a6
Fen1ks18 <dino14sirovica@gmail.com>
2022-04-02 18:00:51 +0200

    Dodao shoping listu - pocetak

--- lib/models/item_list.dart
+++ lib/models/item_list.dart
@@ -4,9 +4,32 @@ import 'package:monkey/models/item.dart';
 // lista za kupovinu npr
 class ItemList {
   final String uid;
-  String name;
-  List<Item> itemList;
-  Category category;
+  String _name;
+  List<Item> _itemList = [];
+  Category _category;
 
-  ItemList(this.uid, this.name, this.itemList, this.category);
+  ItemList(this.uid, this._name, this._category);
+
+  List<Item> get itemList => _itemList;
+
+
+  set itemList(List<Item> value) {
+    _itemList = value;
+  }
+
+  void setItemList(List<Item> value) {
+    _itemList = value;
+  }
+
+  Category get category => _category;
+
+  set category(Category value) {
+    _category = value;
+  }
+
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
 }
