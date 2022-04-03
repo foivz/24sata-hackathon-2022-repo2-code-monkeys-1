6f2ed03639397f1e9fed9adc5d1df605f40b838c
Djaka01 <ivandjakovac55@gmail.com>
2022-04-03 04:12:01 +0200

    Budget Screen - Button

--- lib/pages/create_budget_screen.dart
+++ lib/pages/create_budget_screen.dart
@@ -11,43 +11,49 @@ class _CreateBudgetScreenState extends State<CreateBudgetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey[200],
-        foregroundColor: Colors.grey[600],
-        title: Text('New budget'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.check))
-        ],
-      ),
-      body: Container(
-      margin: EdgeInsets.all(36),
-      child:  Column(
-        children: [
-        const TextField(
-            decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Name',
-        )),
-        const SizedBox(height: 16),
-        const TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Amount',
-          ),
-          keyboardType: TextInputType.number,
+        appBar: AppBar(
+          backgroundColor: Colors.grey[200],
+          foregroundColor: Colors.grey[600],
+          title: Text('New budget'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.check))
+          ],
         ),
-        const SizedBox(height: 16),
-        const TextField(
-            decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Category',
-        )),
-      ]),)
-    );
+        body: Container(
+          margin: EdgeInsets.all(36),
+          child: Column(children: [
+            const TextField(
+                decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Name',
+            )),
+            const SizedBox(height: 16),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Amount',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 16),
+            const TextField(
+                decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Category',
+            )),
+            const SizedBox(height: 16),
+            FlatButton(
+              minWidth: 250,
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: () {},
+                child: Text('Add'))
+          ]),
+        ));
   }
 }
